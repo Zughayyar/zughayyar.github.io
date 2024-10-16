712d7ea803bce031c759194d6dd023fda113b725
Anas Zughayyar <anaszughayyar@neobook.local>
2024-10-16 16:57:38 +0300

    update page

--- Scripts/script-main.js
+++ Scripts/script-main.js
@@ -11,16 +11,16 @@ function redirect_page(element) {
     let project = element.innerText
     console.log("The project chose : " + project)
     if (project == 'Calculator') {
-        location.href="https://zughayyar.github.io/Calculator/"
+        window.open('https://zughayyar.github.io/Calculator/' , '_blank');
     }
         
 
     if (project == 'Dojo Weather') {
-        location.href="https://zughayyar.github.io/Dojo-Weather/"
+        window.open('https://zughayyar.github.io/Dojo-Weather/' , '_blank');
     }
 
     if (project == 'Profile Page') {
-        location.href="https://zughayyar.github.io/Profile-Page/"
+        window.open('https://zughayyar.github.io/Profile-Page/' , '_blank');
     }
     
 }
