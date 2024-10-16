375cfd12c99f3480ca508ef52ef55ca8350bc4ef
Anas Zughayyar <anaszughayyar@neobook.local>
2024-10-16 22:29:51 +0300

    update page

--- Scripts/script-main.js
+++ Scripts/script-main.js
@@ -7,20 +7,26 @@ Date: 25/09/2024
 
 console.log("Page loaded... ")      // load page
 
-function redirect_page(element) {
-    let project = element.innerText
-    console.log("The project chose : " + project)
-    if (project == 'Calculator') {
-        window.open('https://zughayyar.github.io/Calculator/' , '_blank');
-    }
-        
+$("#prj-calculator").click(function() {
+    window.open('https://zughayyar.github.io/Calculator/' , '_blank');
+})
 
-    if (project == 'Dojo Weather') {
-        window.open('https://zughayyar.github.io/Dojo-Weather/' , '_blank');
-    }
+$("#prj-dojo-weather").click(function() {
+    window.open('https://zughayyar.github.io/Dojo-Weather/' , '_blank');
+})
 
-    if (project == 'Profile Page') {
-        window.open('https://zughayyar.github.io/Profile-Page/' , '_blank');
-    }
-    
-}
+$("#prj-profile-page").click(function() {
+    window.open('https://zughayyar.github.io/Profile-Page/' , '_blank');
+})
+
+$("#link-skills").click(function() {
+    window.scrollTo(0, 750);
+})
+
+$("#link-projects").click(function() {
+    window.scrollTo(0, 1400);
+})
+
+$("#prj-likes").click(function() {
+    window.open('https://zughayyar.github.io/Likes/' , '_blank');
+})
