cc310f1221c86a499f436d5d411d9950cc83193a
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-01-29 13:13:25 +0200

    new page

---
+++ Back up/Scripts/script-main.js
@@ -0,0 +1,32 @@
+/*
+file: Profile Page (main style)
+type: Javascript file
+Developer: Anas Zughayyar
+Date: 25/09/2024
+*/
+
+console.log("Page loaded... ")      // load page
+
+$("#prj-calculator").click(function() {
+    window.open('https://zughayyar.github.io/Calculator/' , '_blank');
+})
+
+$("#prj-dojo-weather").click(function() {
+    window.open('https://zughayyar.github.io/Dojo-Weather/' , '_blank');
+})
+
+$("#prj-profile-page").click(function() {
+    window.open('https://zughayyar.github.io/Profile-Page/' , '_blank');
+})
+
+$("#link-skills").click(function() {
+    window.scrollTo(0, 750);
+})
+
+$("#link-projects").click(function() {
+    window.scrollTo(0, 1400);
+})
+
+$("#prj-likes").click(function() {
+    window.open('https://zughayyar.github.io/Likes/' , '_blank');
+})
